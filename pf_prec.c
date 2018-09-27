df99939b01bf59e0c151a17411babb7a4a2ff099
Simon Harris <simonbharris@gmail.com>
2018-09-26 17:19:57 -0700

    Reorganized my precision function, split it into two branches + bugfix

--- pf_prec.c
+++ pf_prec.c
@@ -18,7 +18,7 @@
 ** string only contains zeros, if it does, the represented value is zero.
 */
 
-static int is_zero(char *str)
+static int is_hexzero(char *str)
 {
 	while (*str)
 		if (*str++ != '0')
@@ -27,15 +27,23 @@ static int is_zero(char *str)
 }
 
 /*
-** xp_prepend
-** adds 0x or 0X to the beginning of p and xX datatypes when appropriate.
+** pf_prec_n Precision numeric
+** Pads with zeros up to length of precision.
+** if alt flag is enabled or is a pointer type, prepends 0, 0x, or 0X
+** to the appropriate types.
+** (Handles diouxbp types, with special cases for oxp)
 */
 
-static char *xp_prepend(t_pfdrcv drcv, char **astr)
+static void pf_prec_n(t_pfdrcv drcv, char **astr, char **hold)
 {
+	if (drcv.oflags & PFO_PREC && (int)ft_strlen(*astr) < drcv.pv)
+	{
+		*hold = gen_padding(drcv.pv - ft_strlen(*astr), '0');
+		*astr = ft_strffjoin(hold, astr);
+	}
 	if ((drcv.oflags & PFO_X) && (drcv.oflags & PFO_ALT))
 	{
-		if (!is_zero(*astr))
+		if (!is_hexzero(*astr))
 		{
 			if (drcv.oflags & PFO_CAPS)
 				*astr = ft_strcfjoin("0X", astr);
@@ -45,52 +53,45 @@ static char *xp_prepend(t_pfdrcv drcv, char **astr)
 	}
 	else if (drcv.oflags & PFO_P)
 		*astr = ft_strcfjoin("0x", astr);
-	return (*astr);
+	else if (drcv.oflags & PFO_O && drcv.oflags & PFO_ALT)
+		if (**astr != '0')
+			*astr = ft_strcfjoin("0", astr);
 }
 
 /*
-** o_prepend
-** Entires an octal type ('o') begins with a zero.
+** pf_prec_s string precision
+** Truncates the string so that it is no longer
+** than the precision value (drcv.pv)
 */
 
-static char *o_prepend(t_pfdrcv drcv, char **astr)
+static void pf_prec_s(t_pfdrcv drcv, char **astr, char **hold)
 {
-	if (drcv.oflags & PFO_O)
-		if (**astr != '0')
-			*astr = ft_strcfjoin("0", astr);
-	return (*astr);
+	if ((drcv.oflags & PFO_L) && (int)ft_wstrlen((wchar_t *)*astr) > drcv.pv)
+	{
+		*hold = *astr;
+		*astr = (char *)ft_wstrsub((wchar_t *)*hold, 0, drcv.pv);
+		ft_memdel((void **)hold);
+	}
+	if ((int)ft_strlen(*astr) > drcv.pv)
+	{
+		*hold = *astr;
+		*astr = ft_strsub(*hold, 0, drcv.pv);
+		ft_memdel((void **)hold);
+	}
 }
+
+/*
+** pf_prec - pf precision
+** Directs to either numeric precision, or string precision functions.
+*/
 
 char *pf_prec(t_pfdrcv drcv, char **astr)
 {
 	char *hold;
 
-		if ((drcv.oflags & PFO_DIOUX) || (drcv.oflags & PFO_P))
-		{
-			if (drcv.oflags & PFO_PREC && (int)ft_strlen(*astr) < drcv.pv)
-			{
-				hold = gen_padding(drcv.pv - ft_strlen(*astr), '0');
-				*astr = ft_strffjoin(&hold, astr);
-			}
-			if ((drcv.oflags & PFO_X) || (drcv.oflags & PFO_P))
-				xp_prepend(drcv, astr);
-			else if (drcv.oflags & PFO_O)
-				o_prepend(drcv, astr);
-		}
+		if ((drcv.oflags & (PFO_DIOUXB | PFO_P)))
+			pf_prec_n(drcv, astr, &hold);
 		else if ((drcv.oflags & PFO_PREC) && (drcv.oflags & PFO_S))
-		{
-			if ((drcv.oflags & PFO_L) && (int)ft_wstrlen((wchar_t *)*astr) > drcv.pv)
-			{
-				hold = *astr;
-				*astr = (char *)ft_wstrsub((wchar_t *)hold, 0, drcv.pv);
-				ft_memdel((void **)&hold);
-			}
-			if ((int)ft_strlen(*astr) > drcv.pv)
-			{
-				hold = *astr;
-				*astr = ft_strsub(hold, 0, drcv.pv);
-				ft_memdel((void **)&hold);
-			}
-		}
+			pf_prec_s(drcv, astr, hold);
 	return (*astr);
 }
